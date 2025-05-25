52b4e2f0bc11b3e8dbd41e639d8c62fc955609ea
Xenoyia <xenthekobold@gmail.com>
2025-05-25 14:59:34 +0100

    Refactor to use less reflection

--- Plugin.cs
+++ Plugin.cs
@@ -12,10 +12,40 @@ namespace TaxAssistant
     {
         private static ManualLogSource StaticLogger;
 
+        // Cached reflection info
+        private static Type GameMgrType, TUnitMgrType, PolicyUIType, NpcAlarmUIType, SysMgrType;
+        private static PropertyInfo GameMgrInstanceProp;
+        private static FieldInfo TUnitMgrField, ListCitizenField, PolicyUIField, NpcAlarmUIField, SysMgrField, DayField;
+        private static MethodInfo TaxExecutionMethod, NpcAlarmCallMethod;
+        private static Type AlarmStateType;
+        private static object AlarmStateBasic;
+
         private void Awake()
         {
             StaticLogger = Logger;
-            Logger.LogInfo($"Tax Assistant by Xenoyia loaded.");
+            Logger.LogInfo("Tax Assistant by Xenoyia loaded.");
+
+            // Cache all reflection info here
+            GameMgrType = AccessTools.TypeByName("GameMgr");
+            TUnitMgrType = AccessTools.TypeByName("T_UnitMgr");
+            PolicyUIType = AccessTools.TypeByName("PolicyUI");
+            NpcAlarmUIType = AccessTools.TypeByName("NpcAlarmUI");
+            SysMgrType = AccessTools.TypeByName("SystemMgr");
+
+            GameMgrInstanceProp = GameMgrType.GetProperty("Instance", BindingFlags.Static | BindingFlags.Public);
+            TUnitMgrField = GameMgrType.GetField("_T_UnitMgr", BindingFlags.Instance | BindingFlags.Public);
+            ListCitizenField = TUnitMgrType.GetField("List_Citizen", BindingFlags.Instance | BindingFlags.Public);
+            PolicyUIField = GameMgrType.GetField("_PolicyUI", BindingFlags.Instance | BindingFlags.Public);
+            NpcAlarmUIField = GameMgrType.GetField("_NpcAlarmUI", BindingFlags.Instance | BindingFlags.Public);
+            SysMgrField = GameMgrType.GetField("_SysMgr", BindingFlags.Instance | BindingFlags.Public);
+            DayField = SysMgrType.GetField("m_Day", BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+
+            TaxExecutionMethod = PolicyUIType.GetMethod("TaxExecution", BindingFlags.Instance | BindingFlags.Public);
+            // NpcAlarm_Call signature: (string, bool, AlarmState, int)
+            AlarmStateType = NpcAlarmUIType.GetNestedType("AlarmState");
+            NpcAlarmCallMethod = NpcAlarmUIType.GetMethod("NpcAlarm_Call", new[] { typeof(string), typeof(bool), AlarmStateType, typeof(int) });
+            AlarmStateBasic = Enum.Parse(AlarmStateType, "Basic");
+
             Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
         }
 
@@ -23,19 +53,16 @@ namespace TaxAssistant
         {
             try
             {
-                var gameMgrType = AccessTools.TypeByName("GameMgr");
-                var gameMgr = gameMgrType.GetProperty("Instance", BindingFlags.Static | BindingFlags.Public).GetValue(null);
-                var tUnitMgr = gameMgrType.GetField("_T_UnitMgr", BindingFlags.Instance | BindingFlags.Public).GetValue(gameMgr);
-                var listCitizenField = tUnitMgr.GetType().GetField("List_Citizen", BindingFlags.Instance | BindingFlags.Public);
-                var citizens = listCitizenField.GetValue(tUnitMgr) as System.Collections.IEnumerable;
-                var policyUI = gameMgrType.GetField("_PolicyUI", BindingFlags.Instance | BindingFlags.Public).GetValue(gameMgr);
-                var collectMethod = policyUI.GetType().GetMethod("TaxExecution", BindingFlags.Instance | BindingFlags.Public);
+                var gameMgr = GameMgrInstanceProp.GetValue(null);
+                var tUnitMgr = TUnitMgrField.GetValue(gameMgr);
+                var citizens = (System.Collections.IEnumerable)ListCitizenField.GetValue(tUnitMgr);
+                var policyUI = PolicyUIField.GetValue(gameMgr);
 
                 float totalCollections = 0f;
                 int count = 0;
                 foreach (var citizen in citizens)
                 {
-                    float collected = (float)collectMethod.Invoke(policyUI, new object[] { citizen, false });
+                    float collected = (float)TaxExecutionMethod.Invoke(policyUI, new object[] { citizen, false });
                     totalCollections += collected;
                     count++;
                 }
@@ -43,13 +70,9 @@ namespace TaxAssistant
                 {
                     StaticLogger.LogInfo($"[Tax Assistant] Collection complete. {count} citizens processed. Total: {totalCollections}");
 
-                    var npcAlarmUI = gameMgrType.GetField("_NpcAlarmUI", BindingFlags.Instance | BindingFlags.Public).GetValue(gameMgr);
-                    var npcAlarmType = npcAlarmUI.GetType();
-                    var alarmStateType = npcAlarmType.GetNestedType("AlarmState");
-                    var alarmStateBasic = Enum.Parse(alarmStateType, "Basic");
+                    var npcAlarmUI = NpcAlarmUIField.GetValue(gameMgr);
                     string message = $"<sprite name=FS_Tax> The Tax Assistant has collected <color=#FFE331>{totalCollections:N0}</color> from our ratizens!";
-                    npcAlarmType.GetMethod("NpcAlarm_Call", new[] { typeof(string), typeof(bool), alarmStateType, typeof(int) })
-                        .Invoke(npcAlarmUI, new object[] { message, false, alarmStateBasic, 0 });
+                    NpcAlarmCallMethod.Invoke(npcAlarmUI, new object[] { message, false, AlarmStateBasic, 0 });
                 }
             }
             catch (Exception ex)
@@ -65,22 +88,18 @@ namespace TaxAssistant
 
             static MethodInfo TargetMethod()
             {
-                var systemMgrType = AccessTools.TypeByName("SystemMgr");
-                return systemMgrType?.GetMethod("ProsHappyRefresh", BindingFlags.Instance | BindingFlags.Public);
+                return SysMgrType?.GetMethod("ProsHappyRefresh", BindingFlags.Instance | BindingFlags.Public);
             }
 
             static void Postfix()
             {
-                // Get the current day from the game manager
-                var gameMgrType = AccessTools.TypeByName("GameMgr");
-                var gameMgr = gameMgrType.GetProperty("Instance", BindingFlags.Static | BindingFlags.Public).GetValue(null);
-                var sysMgr = gameMgrType.GetField("_SysMgr", BindingFlags.Instance | BindingFlags.Public).GetValue(gameMgr);
-                int currentDay = (int)sysMgr.GetType().GetField("m_Day", BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public).GetValue(sysMgr);
+                var gameMgr = GameMgrInstanceProp.GetValue(null);
+                var sysMgr = SysMgrField.GetValue(gameMgr);
+                int currentDay = (int)DayField.GetValue(sysMgr);
 
                 if (currentDay != _lastProcessedDay)
                 {
                     _lastProcessedDay = currentDay;
-                    // Call DoCollection directly
                     DoCollection();
                 }
             }
